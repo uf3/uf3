1412ccf5402f09d3b2020c5dda64f71225b94ba6
Hendrik Kraß <hendrik.krass@uni-konstanz.de>
2023-01-12 13:41:13 +0100

    support for automatically applying repulsion correction

--- uf3/util/yaml_tools.py
+++ uf3/util/yaml_tools.py
@@ -8,7 +8,7 @@ import logging
 accepted_file_formats = ['.xyz', '.pkl']
 
 default_settings = {'verbose': 20, 'outputs_path': './outputs', 'elements': 'W', 'degree': 2, 'seed': 0, 'data': {'db_path': 'data.db', 'max_per_file': -1, 'min_diff': 0.0, 'generate_stats': True, 'progress': 'bar', 'vasp_pressure': False, 'sources': {'path': ['./w-14.xyz', './w-14.xyz'], 'pattern': '*'}, 'keys': {'atoms_key': 'geometry', 'energy_key': 'energy', 'force_key': 'force', 'size_key': 'size'}, 'pickle_path': 'data1.pkl'}, 'basis': {'r_min': 0, 'r_max': 5.5, 'resolution': 15, 'fit_offsets': True, 'trailing_trim': 3, 'leading_trim': 0, 'mask_trim': True, 'knot_strategy': 'linear', 'knots_path': 'knots.json', 'load_knots': False, 'dump_knots': False}, 'features': {
-    'db_path': 'data.db', 'features_path': 'features.h5', 'n_cores': 4, 'parallel': 'python', 'fit_forces': True, 'column_prefix': 'x', 'batch_size': 100, 'table_template': 'features_{}'}, 'model': {'model_path': 'model.json'}, 'learning': {'features_path': 'features.h5', 'splits_path': 'splits.json', 'weight': 0.5, 'model_path': 'model.json', 'batch_size': 2500, 'regularizer': {'ridge_1b': 1e-08, 'curvature_1b': 0, 'ridge_2b': 0, 'curvature_2b': 1e-08, 'ridge_3b': 1e-05, 'curvature_3b': 1e-08}}}
+    'db_path': 'data.db', 'features_path': 'features.h5', 'n_cores': 4, 'parallel': 'python', 'fit_forces': True, 'column_prefix': 'x', 'batch_size': 100, 'table_template': 'features_{}'}, 'learning': {'features_path': 'features.h5', 'splits_path': 'splits.json', 'weight': 0.5, 'model_path': 'model.json', 'batch_size': 2500, 'regularizer': {'ridge_1b': 1e-08, 'curvature_1b': 0, 'ridge_2b': 0, 'curvature_2b': 1e-08, 'ridge_3b': 1e-05, 'curvature_3b': 1e-08}, "repulsion_correction": False, "min_curvature": 0.0, "repulsion_suffix": "_repulsion"}}
 
 # Find all files in the current directory
 
@@ -609,6 +609,29 @@ def fit(args):
     model.to_json(model_path)
 
     logging.info(f"Model fit complete and stored to {model_path}.")
+
+    if settings.get('learning', {}).get(
+        'repulsion_correction', default_settings['learning']['repulsion_correction']):
+        from uf3.data import io
+        from uf3.data import analyze
+        data_coordinator = io.DataCoordinator()
+        data_coordinator.load_dataframe(df_data)
+        atoms_key = data_coordinator.atoms_key
+        analyzer = analyze.DataAnalyzer(model.bspline_config.chemical_system, 
+                                        r_cut=10.0,
+                                        bins=0.01)
+        analyzer.load_entries(df_data[atoms_key])
+        analysis = analyzer.analyze()
+        min_curvature = settings.get('learning', {}).get(
+            'min_curvature', default_settings['learning']['min_curvature'])
+        for pair in analysis["lower_bounds"].keys():
+            model.fix_repulsion_2b(pair, r_target=analysis["lower_bounds"][pair], min_curvature=min_curvature)
+
+        
+        model_repulsion_suffix = settings.get('learning', {}).get(
+            'repulsion_suffix', default_settings['learning']['repulsion_suffix'])
+
+        model.to_json("".join(model_path.split(".")[:-1]) + model_repulsion_suffix + ".json")
 
 
 def predict(args):
